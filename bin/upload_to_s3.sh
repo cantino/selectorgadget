31f0df1989009e9030a3eadb550fa370dcf80613
Andrew Cantino <cantino@gmail.com>
2013-06-02 16:05:31 -0700

    update

--- bin/upload_to_s3.sh
+++ bin/upload_to_s3.sh
@@ -7,7 +7,7 @@ else
   git clone git@github.com:cantino/selectorgadget.git tmp-sg
   cd tmp-sg
 fi
-s3cmd sync --delete-removed --rexclude=^.git\|^sites\|^test . s3://selectorgadget.com/unstable/
-s3cmd put doc/* s3://selectorgadget.com
+s3cmd sync --cf-invalidate --delete-removed --rexclude=^.git\|^sites\|^test . s3://selectorgadget.com/unstable/
+s3cmd put --cf-invalidate doc/* s3://selectorgadget.com
 git reset --hard && git clean -df && git checkout 0.4.1
-s3cmd sync --delete-removed --rexclude=^.git\|^sites\|^test . s3://selectorgadget.com/stable/
+s3cmd sync --cf-invalidate --delete-removed --rexclude=^.git\|^sites\|^test . s3://selectorgadget.com/stable/
