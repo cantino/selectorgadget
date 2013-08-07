2c723381e17b22ae2667eba5c16ce8148f3cc9f3
Andrew Cantino <cantino@gmail.com>
2013-08-07 12:28:08 -0700

    verify site; add favicon

--- bin/bundle_chrome.sh
+++ bin/bundle_chrome.sh
@@ -2,3 +2,5 @@
 
 cp build/selectorgadget_combined.css chrome/combined.css
 cat chrome/header.js build/selectorgadget_combined.js chrome/footer.js > chrome/combined.js
+rm -f extension.zip
+zip -r extension.zip chrome
