abd2c438120bbd86c006ba31926361d88d0e35c9
Joseph Ramos <joseph.ramos@taller.net.br>
2014-11-03 14:56:48 -0500

    Removed unnecesary files.

--- chrome/background.js
+++
@@ -1,4 +0,0 @@
-chrome.browserAction.onClicked.addListener(function(tab) {
-  chrome.tabs.insertCSS(tab.id, { file: "combined.css" });
-  chrome.tabs.executeScript(tab.id, { file: "combined.js" });
-});
