3da801e84d5c68b14ecfeeae952df7877d2e0e21
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2015-03-05 16:36:24 -0300

    High improvement to release bumper tasks.

--- gulpfile.js
+++ gulpfile.js
@@ -17,9 +17,19 @@ var fs          = require('fs')
   , bump        = require('gulp-bump')
   , filter      = require('gulp-filter')
   , tag         = require('gulp-tag-version')
-  , pkg         = require('./package.json')
+  , sequence    = require('run-sequence')
   , signature   = fs.readFileSync('./signature')
-  , gitignore   = fs.existsSync('.gitignore') ? fs.readFileSync('.gitignore') + '' : '';
+  , gitignore   = fs.existsSync('.gitignore') ? fs.readFileSync('.gitignore') + '' : ''
+    // We read it as file so not to cache results.
+  , pkg         = JSON.parse(fs.readFileSync('./package.json'));
+
+/**
+ * Refresh package info.
+ */
+function refreshPackage(next) {
+  pkg = JSON.parse(fs.readFileSync('./package.json'));
+  next && next();
+}
 
 /**
  * Get signature header.
@@ -64,40 +74,40 @@ gulp.task('css', function () {
 });
 
 /**
- * Bumping version number and tagging the repository with it.
- * Please read http://semver.org/
- *
- * You can use the commands
- *
- *     gulp patch     # makes v0.1.0 → v0.1.1
- *     gulp feature   # makes v0.1.1 → v0.2.0
- *     gulp release   # makes v0.2.1 → v1.0.0
- *
- * To bump the version numbers accordingly after you did a patch,
- * introduced a feature or made a backwards-incompatible release.
+ * Bumps version numbers.
  */
-function bumper(importance) {
-  // get all the files to bump version in 
-  return gulp.src(['./package.json', './bower.json'])
-    // bump the version number in those files 
-    .pipe(bump({type: importance}))
-    // save it back to filesystem
-    .pipe(gulp.dest('./'));
+function bumper(importance, end) {
+  // Get all the files to bump version in.
+  gulp.src(['./package.json', './bower.json'])
+    // Bump the version number in those files.
+    .pipe(bump({ type: importance }))
+    // Save it back to filesystem.
+    .pipe(gulp.dest('./'))
+    // Update our copy of package json.
+    .on('end', refreshPackage.bind(this, end));
 }
 
-gulp.task('release', function () {
-  var version = require('./package.json').version;
+/**
+ * Creates a new release.
+ */
+function release(version, end) {
+  sequence(version, 'build', 'publish', end);
+}
 
+/**
+ * Commits files and publishes a new release.
+ */
+gulp.task('publish', function () {
   return gulp.src('./*')
     .pipe(ignore.include(gitignore))
     // Add all files.
     .pipe(git.add({ args: '-u' }))
     // Commit the changed version number.
-    .pipe(git.commit('Created release v' + version))
+    .pipe(git.commit('Created release v' + pkg.version))
     // Read only one file to get the version number.
-    // .pipe(filter('package.json'))
+    .pipe(filter('package.json'))
     // Tag it to the repository. 
-    // .pipe(tag())
+    .pipe(tag())
 });
 
 // Versioning tags.
@@ -109,12 +119,12 @@ gulp.task('version:major', bumper.bind(this, 'major'));
 gulp.task('version', ['version:minor']);
 
 // Releasing tasks.
-gulp.task('push:patch',   ['version:patch'], ['build'], ['release']);
-gulp.task('push:feature', ['version:minor'], ['build'], ['release']);
-gulp.task('push:release', ['version:major'], ['build'], ['release']);
+gulp.task('release:patch', release.bind(this, 'version:patch'));
+gulp.task('release:minor', release.bind(this, 'version:minor'));
+gulp.task('release:major', release.bind(this, 'version:major'));
 
 // Common alias.
-gulp.task('push', ['push:feature']);
+gulp.task('release', ['release:minor']);
 
 // Compilation tasks.
 gulp.task('build', ['css', 'script']);
